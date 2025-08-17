a5dc3310c099622339b5d7bd4fb3be7f1f18345c
Abdelrahman <151108921+Mordekai66@users.noreply.github.com>
2025-08-17 12:35:01 +0300

    Add files via upload

---
+++ decrypt.py
@@ -0,0 +1,26 @@
+from cryptography.fernet import Fernet
+import os
+
+key = None
+
+with open("key.key", "rb") as f:
+    key = f.read()
+
+f = Fernet(key)
+
+dir_base = os.listdir()
+print("Decrypting files in the current directory:")
+print(dir_base)
+print("Decrypting files...")
+
+for file in dir_base:
+    if os.path.isfile(file):
+        if file != "encrypt.py" and file != "key.key" and file != "decrypt.py":
+            with open(file, "rb") as f_in:
+                encrypted_data = f_in.read()
+            decrypted_data = f.decrypt(encrypted_data)
+            with open(file, "wb") as f_out:
+                f_out.write(decrypted_data)
+
+print("Decryption complete.")
+print("Files decrypted successfully.")
---
+++ encrypt.py
@@ -0,0 +1,28 @@
+from cryptography.fernet import Fernet
+import os
+
+key = Fernet.generate_key()
+
+with open("key.key", "wb") as f:
+    f.write(key)
+
+f = Fernet(key)
+
+dir_base = os.listdir()
+print("Files in the current directory:")
+print(dir_base)
+print("Encrypting these files...")
+
+for file in dir_base:
+    if os.path.isfile(file):
+        if file != "encrypt.py" and file != "key.key" and file != "decrypt.py":
+            with open(file, "rb") as f_in:
+                data = f_in.read()
+            
+            encrypted_data = f.encrypt(data)
+            
+            with open(file, "wb") as f_out:
+                f_out.write(encrypted_data)
+
+print("Encryption complete.")
+print("Files encrypted successfully.")
