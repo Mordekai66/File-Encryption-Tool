4811f1e85e120dda87825ce378965a9042195888
Abdelrahman <151108921+Mordekai66@users.noreply.github.com>
2025-08-17 12:57:28 +0300

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -13,6 +13,12 @@ print("Decrypting files in the current directory:")
 print(dir_base)
 print("Decrypting files...")
 
+
+ask = input("Are you sure...? (y/n)")
+
+if ask == "n":
+    exit()
+
 for file in dir_base:
     if os.path.isfile(file):
         if file != "encrypt.py" and file != "key.key" and file != "decrypt.py":
